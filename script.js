a7782e1675b18ad94f57d10a77dc93e4f36373a8
Kavin <pinghangfan1022@gmail.com>
2021-07-05 22:27:53 +1200

    Resize button now resize within the origin 480x480 grid, renamed multiple functions in js code

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function createGrid(){
-    for(let i = 0; i < 16; i++){
-        var container = document.getElementById("grid-container");
+function createGrid(rowNumber){
+    if (rowNumber > 100){
+        rowNumber = 100;
+    }
+    size = 480 / rowNumber;
+
+    var container = document.getElementById("grid-container");
+    for(let i = 0; i < rowNumber; i++){
         var row = document.createElement("div");
         row.className = "inner-container";
-        row.id = "row" + i.toString()
+        row.id = "row" + i.toString();
         container.appendChild(row);
 
-        for(let j = 0; j < 16; j++){
+        for(let j = 0; j < rowNumber; j++){
             var rowContainer = document.getElementById("row" + i.toString());
             var cell = document.createElement("div");
             cell.className = "col";
+            cell.style.height = size + "px"
+            cell.style.width = size + "px"
             rowContainer.appendChild(cell);
         }
     }
@@ -17,20 +24,23 @@ function createGrid(){
 
 function changeColour(){
     const square = document.getElementsByClassName("col");
-    for (var i = 0; i < square.length; i++) {
-        square[i].addEventListener('mouseenter', function(event){
-            event.target.style.backgroundColor = "black"
-        });
-    }
 
     for (var i = 0; i < square.length; i++) {
         square[i].addEventListener('mouseenter', function(event){
-            event.target.style.backgroundColor = "blue"
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            event.target.style.backgroundColor = "#" + randomColor;
         });
     }
 }
 
 function clearGrid(){
+    var container = document.getElementById("grid-container");
+    while (container.firstChild){
+        container.removeChild(container.lastChild)
+    }
+}
+
+function clearButton(){
     const button = document.getElementById("clearButton");
     button.addEventListener("click", function(){
         const grid = document.getElementsByClassName("col");
@@ -40,9 +50,20 @@ function clearGrid(){
     });
 }
 
+function resizeButton(){
+    const button = document.getElementById("resizeButton");
+    button.addEventListener("click", function(){
+        let newSize = window.prompt("Enter new grid size: ")
+        clearGrid();
+        createGrid(newSize);
+        changeColour();
+    });
+}
+
 function main(){
-    createGrid();
+    createGrid(16);
     changeColour();
-    clearGrid();
+    clearButton();
+    resizeButton();
 }
 main()
